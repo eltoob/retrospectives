9fd0c2bffb890eb2456f07f32fda7cd5407de210
Adam Berlin <berlin.ab@gmail.com>
2012-05-13 13:08:30 -0400

    Added meh and sad items.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,25 @@
 class Item < ActiveRecord::Base
   HAPPY = 'happy'
+  MEH = 'meh'
+  SAD = 'sad'
 
   attr_accessible :category, :description
 
   def self.build_happy
     new.tap do |item|
       item.category = HAPPY
+    end
+  end
+
+  def self.build_meh
+    new.tap do |item|
+      item.category = MEH
+    end
+  end
+
+  def self.build_sad
+    new.tap do |item|
+      item.category = SAD
     end
   end
 end
--- app/models/retro.rb
+++ app/models/retro.rb
@@ -1,5 +1,8 @@
 class Retro < ActiveRecord::Base
   has_many :items
+  has_many :happy_items, conditions: { :category => Item::HAPPY }, class_name: 'Item'
+  has_many :meh_items, conditions: { :category => Item::MEH }, class_name: 'Item'
+  has_many :sad_items, conditions: { :category => Item::SAD }, class_name: 'Item'
 
   def create_item(attrs = {})
     item = Item.new(attrs)
