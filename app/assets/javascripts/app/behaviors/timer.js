5d07e55ca3bb393e039e7eea7bcf30ca434cc9d0
Adam Berlin <berlin.ab@gmail.com>
2012-05-14 23:00:06 -0400

    Color the timer.

--- app/assets/javascripts/app/behaviors/timer.js
+++ app/assets/javascripts/app/behaviors/timer.js
@@ -1,4 +1,6 @@
 Retrospective.Behaviors.Timer = function(container){
+  HALF_AN_HOUR_IN_SECONDS = 1800
+
   var seconds = container.data('seconds-left');
   var retros_path = container.data("retros-path");
   var later = seconds.seconds().fromNow();
@@ -9,6 +11,11 @@ Retrospective.Behaviors.Timer = function(container){
     seconds_left = val[6];
 
     total_seconds_left = hours_left + minutes_left + seconds_left;
+
+    if(total_seconds_left < HALF_AN_HOUR_IN_SECONDS){
+      $(".hasCountdown").addClass("red");
+    }
+
     $.post(retros_path, {_method: 'put', retro: {seconds_left: total_seconds_left}})
   };
 
