8da1a64b9d4fd48ce589ce4723d9beba06bf8a8a
Adam Berlin <berlin.ab@gmail.com>
2012-05-15 00:15:36 -0400

    more healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Retrospective::Application.routes.draw do
   root :to => 'welcome#index'
   resources :retros, only: [:create, :show, :update]
   resources :items, only: [:create, :update, :destroy]
+  match "/health_check" => "welcome#health_check"
 end
