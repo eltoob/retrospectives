915e5625b4dc15c4f4e25971ae6ad16257865f37
Adam Berlin <berlin.ab@gmail.com>
2012-05-18 08:34:16 -0400

    Removed strange arrow-key keybindings.

--- app/assets/javascripts/app/behaviors/keybindings.js
+++ app/assets/javascripts/app/behaviors/keybindings.js
@@ -12,57 +12,9 @@ Retrospective.Behaviors.Keybindings = function(item){
     }
   };
 
-  var upArrowPressed = function(){
-    var newItem = item.closest("form").prevAll(".retro-item-form:first")
-
-    if(newItem.length > 0) {
-      newItem.find(".retro-item").focus();
-    }
-  };
-
-  var downArrowPressed = function(){
-    var newItem = item.closest("form").nextAll(".retro-item-form:first")
-
-    if(newItem.length > 0) {
-      newItem.find(".retro-item").focus();
-    }
-  };
-
-  var leftArrowPressed = function(){
-    var newItem = item.closest(".swimming-lane").prev();
-
-    if(newItem.length > 0) {
-      newItem.find(".retro-item:first").focus();
-    }
-  };
-
-  var rightArrowPressed = function(){
-    var newItem = item.closest(".swimming-lane").next();
-
-    if(newItem.length > 0) {
-      newItem.find(".retro-item:first").focus();
-    }
-  };
-
   item.keyup(function(event){
     if(event.which == ENTER){
       enterPressed();
-    }
-
-    if(event.which == UP_ARROW){
-      upArrowPressed();
-    }
-
-    if(event.which == DOWN_ARROW){
-      downArrowPressed();
-    }
-
-    if(event.which == LEFT_ARROW){
-      leftArrowPressed();
-    }
-
-    if(event.which == RIGHT_ARROW){
-      rightArrowPressed();
     }
   });
 };
